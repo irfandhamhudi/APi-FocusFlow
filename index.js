62b7ab242edb35b184382ebfa79ec7575e196503
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-05-27 01:06:14 +0700

    cors fix

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["https://focus-flow-app-rho.vercel.app/login"],
+    origin: process.env.CLIENT_URL || "http://localhost:3000", // URL frontend
     credentials: true, // Membolehkan cookies dikirimkan
   })
 );
