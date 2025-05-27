3b9ff94a4cc0c8ecd95a02bcccd426fe2800f1fe
irfandhamhudi <irfandhamhudi712@gmail.com>
2025-05-27 09:08:07 +0700

    cors fix

--- index.js
+++ index.js
@@ -36,8 +36,8 @@ const startServer = async () => {
 
     const PORT = process.env.PORT || 5000;
 
-    const server = app.listen(PORT, () =>
-      console.log(`Server running on port ${PORT}`)
+    const server = app.listen(PORT, "0.0.0.0", () =>
+      console.log(`Server running on port ${PORT}, host 0.0.0.0`)
     );
 
     // Handle SIGTERM for graceful shutdown
